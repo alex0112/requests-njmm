473ee41060cb2925563429cf21ac6e601cc9c336
hagoth <njmm.office@gmail.com>
2016-02-26 18:19:00 -0500

    Created referrals scaffold.  I wasn't planning on doing this until later, but I accidently commited broken code to prod... I manually went in and dropped the old requests table (on the server) but rake won't work via ssh, so I'm creating this new scaffold just so that I can run a migration.  Cheap I know.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160226223035) do
+ActiveRecord::Schema.define(version: 20160226231620) do
+
+  create_table "referrals", force: true do |t|
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "requests", force: true do |t|
     t.string   "name"
