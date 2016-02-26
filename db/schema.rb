118f9f480898aab63fd572bf9fb08c8fc45f7fc4
hagoth <njmm.office@gmail.com>
2016-02-26 18:06:44 -0500

    I blew away the requests scaffold and re-created it.  Here are the apropriate migrations.  (I opened rails db and dropped the old table manually in order to make this work.)

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160226181641) do
+ActiveRecord::Schema.define(version: 20160226223035) do
 
   create_table "requests", force: true do |t|
     t.string   "name"
