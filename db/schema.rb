194e25deb9599757ff643fdf609ddb51bee2dde7
hagoth <njmm.office@gmail.com>
2016-03-16 14:29:20 -0400

    Schema file for the area class

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160226231620) do
+ActiveRecord::Schema.define(version: 20160311231912) do
+
+  create_table "areas", force: true do |t|
+    t.string   "name"
+    t.string   "email"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "referrals", force: true do |t|
     t.datetime "created_at"
